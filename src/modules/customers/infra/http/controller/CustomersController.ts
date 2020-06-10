d12a5f4d51d9478eff38db29dece0c671ae6805a
Fábio <fsmaiorano@gmail.com>
2020-06-10 07:25:28 +0100

    implemente api

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -7,5 +7,14 @@ import { container } from 'tsyringe';
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body;
+
+    const customerCreateService = container.resolve(CreateCustomerService);
+
+    const customer = await customerCreateService.execute({
+      name,
+      email,
+    });
+
+    return response.status(200).json(customer);
   }
 }
